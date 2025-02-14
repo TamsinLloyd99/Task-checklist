d1ca744ed2e55295603f0a26095fdb2d25e24bdb
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2025-02-14 16:04:40 +0000

    added header and footer files

---
+++ Task-checklist/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+return(
+    <footer>
+        <p>&copy; {new Date().getFullYear()} Tamsin Lloyd. All rights reserved.</p>
+    </footer>
+)
+}
+
+export default Footer
---
+++ Task-checklist/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="#">My To Do List</a>
+      </h1>
+      <nav>
+        <ul>
+          <li><a href ="#">New List</a></li>
+        </ul>
+      </nav>
+      <hr />
+    </header>
+  );
+}
+
+export default Header;
